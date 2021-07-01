d082c543d1b054c8a9896939486f102546f2661c
Simeon Dochev <simeondochev@Simeons-MacBook-Air.local>
2021-07-01 19:57:11 +0300

    updated solutions

--- Python OOP/First Steps in OOP - Exercise/05-Flower.py
+++ Python OOP/First Steps in OOP - Exercise/05-Flower.py
@@ -1,8 +1,8 @@
 class Flower:
-    def __init__(self, name, water_requirements, is_happy=False):
+    def __init__(self, name, water_requirements):
         self.name = name
         self.water_requirements = water_requirements
-        self.is_happy = is_happy
+        self.is_happy = False
 
     def water(self, quantity):
         if quantity >= self.water_requirements:
