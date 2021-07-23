856b7aa568f336020b3ea85e096f4da23125b304
Simeon Dochev <simeon.dochev16@gmail.com>
2021-07-23 16:46:17 +0300

    uploaded solved exam

---
+++ Python OOP/Example Exams/Python OOP Retake Exam - 22 Aug 2020/01. Structure (50p)_Skeleton/project/rooms/old_couple.py
@@ -0,0 +1,17 @@
+from project.appliances.fridge import Fridge
+from project.appliances.stove import Stove
+from project.appliances.tv import TV
+from project.rooms.room import Room
+
+
+class OldCouple(Room):
+    _COST = 15
+    _MEMBERS_COUNT = 2
+    _APPLIANCES = [TV(), Fridge(), Stove()]
+
+    def __init__(self, family_name, pension_one, pension_two):
+        super().__init__(family_name, pension_one + pension_two, OldCouple._MEMBERS_COUNT)
+        self.room_cost = OldCouple._COST
+        self.appliances = OldCouple._APPLIANCES * OldCouple._MEMBERS_COUNT
+
+        self.calculate_expenses(self.appliances)
---
+++ Python OOP/Example Exams/Python OOP Retake Exam - 22 Aug 2020/01. Structure (50p)_Skeleton/project/rooms/room.py
@@ -0,0 +1,24 @@
+class Room:
+    _DAYS_IN_MONTH = 30
+
+    def __init__(self, family_name, budget: float, members_count: int):
+        self.family_name = family_name
+        self.budget = budget
+        self.members_count = members_count
+        self.children = []
+
+    @property
+    def expenses(self):
+        return self.__expenses
+
+    @expenses.setter
+    def expenses(self, value):
+        if value < 0:
+            raise ValueError("Expenses cannot be negative")
+        self.__expenses = value
+
+    def calculate_expenses(self, *args):
+        result = 0
+        for li in args:
+            result += sum([el.cost for el in li])
+        self.expenses = result * Room._DAYS_IN_MONTH
