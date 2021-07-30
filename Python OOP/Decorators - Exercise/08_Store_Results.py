679b1814bf73fb4782fab56aed32d0a87d9044cf
Simeon Dochev <simeon.dochev16@gmail.com>
2021-07-30 17:25:25 +0300

    updated solution

--- Python OOP/Decorators - Exercise/08_Store_Results.py
+++ Python OOP/Decorators - Exercise/08_Store_Results.py
@@ -1,15 +1,9 @@
-import os
-
-
 class store_results:
     def __init__(self, function):
         self.function = function
 
     def __call__(self, *args, **kwargs):
-        if not os.path.exists("./results.txt"):
-            with open("results.txt", 'w') as file:
-                pass
-        with open("results.txt", 'a') as file:
+        with open("results.txt", 'a+') as file:
             file.writelines(f"Function {self.function.__name__} was add called. Result: {self.function(*args)}\n")
 
 
